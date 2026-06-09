bd1424c91fb9bcc9bbd535b1804c25188bef41df
hyun0-25 <lisa1072@skku.edu>
2026-06-09 11:24:47 +0900

    [Init] 카드 프로젝트 생성

---
+++ src/main/java/com/example/project_card/ProjectCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCardApplication.class, args);
+    }
+
+}
